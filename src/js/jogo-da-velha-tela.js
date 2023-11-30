03cbbfa78af6ffe16e9c023bb00ba423ae906569
Tiago Riego <tiago.riego@gmail.com>
2023-11-30 19:25:42 -0300

    Layout and game improvements. (#7)
    Co-authored-by: Tiago Riego <tiago.riego@yandeh.com.br>

--- src/js/jogo-da-velha-tela.js
+++ src/js/jogo-da-velha-tela.js
@@ -1,25 +1,29 @@
 (function () {
   const jogoDaVelha = new JogoDaVelha();
-  const jogada = document.querySelectorAll('[id*=jogada-]');
-  const jogador = document.querySelectorAll('[id*=jogador-]');
-  const rodada = document.querySelector('[id=rodada]');
-  rodada.textContent = jogoDaVelha.obterRodada();
+  const jogada = document.querySelectorAll("[id*=jogada-]");
+  const jogador = document.querySelectorAll("[id*=jogador-]");
+  const rodada = document.querySelector("[id=rodada]");
+  const jogando = document.querySelector("[id=jogando");
+  const placar = document.querySelectorAll("[id*=placar-]");
+
+  atualizarRodada();
+  atualizarProximoJogador();
 
   jogada.forEach((el, i) => {
-    el.addEventListener('click', () => {
+    el.addEventListener("click", () => {
       const jogo = jogoDaVelha.obterJogo();
       if (!jogo[i]) {
         jogoDaVelha.jogar(i);
-        const sn = document.createElement('span');
+        const sn = document.createElement("span");
         sn.textContent = jogoDaVelha.obterJogador().jogador;
         el.appendChild(sn);
       }
 
       if (jogoDaVelha.haVencedor()) {
         const vencedor = jogoDaVelha.obterVencedor();
-        if (vencedor.jogador == 'x') {
+        if (vencedor.jogador == "x") {
           jogador[0].textContent = vencedor.pontuacao;
-        } else if (vencedor.jogador == 'o') {
+        } else if (vencedor.jogador == "o") {
           jogador[1].textContent = vencedor.pontuacao;
         }
         reiniciarJogo();
@@ -29,16 +33,34 @@
           reiniciarJogo();
         }
       }
+
+      atualizarProximoJogador();
     });
   });
 
   function reiniciarJogo() {
     jogoDaVelha.reiniciarJogo();
-    rodada.textContent = jogoDaVelha.obterRodada();
     jogada.forEach((el, i) => {
       if (el.firstElementChild) {
         el.removeChild(el.firstElementChild);
       }
     });
+    atualizarRodada();
+  }
+
+  function atualizarRodada() {
+    rodada.textContent = jogoDaVelha.obterRodada();
+  }
+
+  function atualizarProximoJogador() {
+    let jogada = jogoDaVelha.obterProximoJogador().jogador;
+    if (jogada == "x") {
+      placar[1].classList.remove("next-player");
+      placar[0].classList.add("next-player");
+    } else {
+      placar[0].classList.remove("next-player");
+      placar[1].classList.add("next-player");
+    }
+    jogando.innerText = jogada.toUpperCase();
   }
 })();
